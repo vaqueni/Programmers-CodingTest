5fcb3c7313c4ba583b6a7398af60ad80034f8319
sungsan <sungsan1204@gmail.com>
2023-06-09 09:21:49 +0900

    짝지어 제거하기

--- Level2/짝지어 제거하기/Solution.java
+++ Level2/짝지어 제거하기/Solution.java
@@ -1,3 +1,21 @@
-class Solution{
-    
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {   
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+
+        for(int i = 0 ; i < arr.length ; i++){
+            if(stack.isEmpty()==true){
+                stack.push(arr[i]);
+            }else{
+                if(stack.peek() == arr[i]) stack.pop();
+                else stack.push(arr[i]);
+            }
+        }
+
+
+        return stack.isEmpty() ? 1 : 0;
+    }
 }
