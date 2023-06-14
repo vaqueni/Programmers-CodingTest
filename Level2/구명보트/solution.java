86a9a31783aa6b278ecaccdff804329e720dba7d
sungsan <sungsan1204@gmail.com>
2023-06-14 10:21:19 +0900

    구명보트

---
+++ Level2/구명보트/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int count = 0;
+        Arrays.sort(people);
+        int i = 0;
+        int j = people.length-1;
+        while(i<=j){
+            if(people[i] + people[j] <= limit){
+                i++;
+                j--;
+            }else{
+                j--;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
