07234d22070e40d033eabf993bc3898f6a4b3208
sungsan <sungsan1204@gmail.com>
2023-07-03 18:00:22 +0900

    타겟넘버

---
+++ Level2/타겟넘버/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    int[] numbers;
+    int target;
+    int answer;
+    
+    void dfs(int index, int sum){
+        if(index == numbers.length){
+            if(sum == target) answer++;
+            return;
+        }
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        
+        dfs(0, 0);
+        return this.answer;
+    }
+}
